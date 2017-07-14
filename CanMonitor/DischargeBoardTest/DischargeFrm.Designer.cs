66fb337b3710c9927388a571072fb12ebb8acd9d
Robin Cornelius <robin.cornelius@gmail.com>
2017-07-14 14:56:47 +0100

    Update Discharge board plugin

--- CanMonitor/DischargeBoardTest/DischargeFrm.Designer.cs
+++ CanMonitor/DischargeBoardTest/DischargeFrm.Designer.cs
@@ -44,11 +44,30 @@
             this.button_unlock = new System.Windows.Forms.Button();
             this.numericUpDown_node = new System.Windows.Forms.NumericUpDown();
             this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown_selectedfixture = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numericUpDown_dualsplit = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.numericUpDown_pwmon = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown_pwmoff = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.numericUpDown_pwmmax = new System.Windows.Forms.NumericUpDown();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button_setpwm = new System.Windows.Forms.Button();
+            this.numericUpDown_pretrig = new System.Windows.Forms.NumericUpDown();
+            this.label12 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_duration1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_duration2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_duration3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_duration4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_node)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_selectedfixture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_dualsplit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_pwmon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_pwmoff)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_pwmmax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_pretrig)).BeginInit();
             this.SuspendLayout();
             // 
             // numericUpDown_duration1
@@ -68,7 +87,7 @@
             this.numericUpDown_duration1.Size = new System.Drawing.Size(126, 20);
             this.numericUpDown_duration1.TabIndex = 0;
             this.numericUpDown_duration1.Value = new decimal(new int[] {
-            50000,
+            200,
             0,
             0,
             0});
@@ -108,7 +127,7 @@
             this.numericUpDown_duration2.Size = new System.Drawing.Size(126, 20);
             this.numericUpDown_duration2.TabIndex = 2;
             this.numericUpDown_duration2.Value = new decimal(new int[] {
-            50000,
+            200,
             0,
             0,
             0});
@@ -116,7 +135,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 123);
+            this.label3.Location = new System.Drawing.Point(22, 114);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(81, 13);
             this.label3.TabIndex = 5;
@@ -124,7 +143,7 @@
             // 
             // numericUpDown_duration3
             // 
-            this.numericUpDown_duration3.Location = new System.Drawing.Point(109, 121);
+            this.numericUpDown_duration3.Location = new System.Drawing.Point(109, 112);
             this.numericUpDown_duration3.Maximum = new decimal(new int[] {
             349520,
             0,
@@ -139,7 +158,7 @@
             this.numericUpDown_duration3.Size = new System.Drawing.Size(126, 20);
             this.numericUpDown_duration3.TabIndex = 4;
             this.numericUpDown_duration3.Value = new decimal(new int[] {
-            50000,
+            200,
             0,
             0,
             0});
@@ -147,7 +166,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(22, 159);
+            this.label4.Location = new System.Drawing.Point(22, 142);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(81, 13);
             this.label4.TabIndex = 7;
@@ -155,7 +174,7 @@
             // 
             // numericUpDown_duration4
             // 
-            this.numericUpDown_duration4.Location = new System.Drawing.Point(109, 157);
+            this.numericUpDown_duration4.Location = new System.Drawing.Point(109, 140);
             this.numericUpDown_duration4.Maximum = new decimal(new int[] {
             349520,
             0,
@@ -170,14 +189,14 @@
             this.numericUpDown_duration4.Size = new System.Drawing.Size(126, 20);
             this.numericUpDown_duration4.TabIndex = 6;
             this.numericUpDown_duration4.Value = new decimal(new int[] {
-            50000,
+            200,
             0,
             0,
             0});
             // 
             // button_setduration
             // 
-            this.button_setduration.Location = new System.Drawing.Point(76, 204);
+            this.button_setduration.Location = new System.Drawing.Point(76, 250);
             this.button_setduration.Name = "button_setduration";
             this.button_setduration.Size = new System.Drawing.Size(88, 24);
             this.button_setduration.TabIndex = 8;
@@ -191,8 +210,9 @@
             this.comboBox_mode.Items.AddRange(new object[] {
             "Single",
             "Dual",
-            "Quad"});
-            this.comboBox_mode.Location = new System.Drawing.Point(109, 251);
+            "Quad",
+            "Dual Split"});
+            this.comboBox_mode.Location = new System.Drawing.Point(76, 297);
             this.comboBox_mode.Name = "comboBox_mode";
             this.comboBox_mode.Size = new System.Drawing.Size(140, 21);
             this.comboBox_mode.TabIndex = 9;
@@ -201,7 +221,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(22, 254);
+            this.label5.Location = new System.Drawing.Point(22, 300);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(34, 13);
             this.label5.TabIndex = 10;
@@ -209,7 +229,7 @@
             // 
             // button_setmode
             // 
-            this.button_setmode.Location = new System.Drawing.Point(76, 293);
+            this.button_setmode.Location = new System.Drawing.Point(76, 339);
             this.button_setmode.Name = "button_setmode";
             this.button_setmode.Size = new System.Drawing.Size(88, 24);
             this.button_setmode.TabIndex = 11;
@@ -262,17 +282,227 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(32, 19);
+            this.label6.Location = new System.Drawing.Point(22, 14);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(55, 13);
             this.label6.TabIndex = 15;
             this.label6.Text = "Can Node";
+            // 
+            // numericUpDown_selectedfixture
+            // 
+            this.numericUpDown_selectedfixture.Location = new System.Drawing.Point(481, 166);
+            this.numericUpDown_selectedfixture.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown_selectedfixture.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_selectedfixture.Name = "numericUpDown_selectedfixture";
+            this.numericUpDown_selectedfixture.Size = new System.Drawing.Size(126, 20);
+            this.numericUpDown_selectedfixture.TabIndex = 16;
+            this.numericUpDown_selectedfixture.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(288, 168);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Selected fixture";
+            // 
+            // numericUpDown_dualsplit
+            // 
+            this.numericUpDown_dualsplit.Location = new System.Drawing.Point(109, 166);
+            this.numericUpDown_dualsplit.Maximum = new decimal(new int[] {
+            349520,
+            0,
+            0,
+            0});
+            this.numericUpDown_dualsplit.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_dualsplit.Name = "numericUpDown_dualsplit";
+            this.numericUpDown_dualsplit.Size = new System.Drawing.Size(126, 20);
+            this.numericUpDown_dualsplit.TabIndex = 18;
+            this.numericUpDown_dualsplit.Value = new decimal(new int[] {
+            1875,
+            0,
+            0,
+            0});
+            this.numericUpDown_dualsplit.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(22, 168);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(80, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Dual Split delay";
+            // 
+            // numericUpDown_pwmon
+            // 
+            this.numericUpDown_pwmon.Location = new System.Drawing.Point(481, 12);
+            this.numericUpDown_pwmon.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numericUpDown_pwmon.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_pwmon.Name = "numericUpDown_pwmon";
+            this.numericUpDown_pwmon.Size = new System.Drawing.Size(126, 20);
+            this.numericUpDown_pwmon.TabIndex = 20;
+            this.numericUpDown_pwmon.Value = new decimal(new int[] {
+            38,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown_pwmoff
+            // 
+            this.numericUpDown_pwmoff.Location = new System.Drawing.Point(481, 38);
+            this.numericUpDown_pwmoff.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numericUpDown_pwmoff.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_pwmoff.Name = "numericUpDown_pwmoff";
+            this.numericUpDown_pwmoff.Size = new System.Drawing.Size(126, 20);
+            this.numericUpDown_pwmoff.TabIndex = 21;
+            this.numericUpDown_pwmoff.Value = new decimal(new int[] {
+            40,
+            0,
+            0,
+            0});
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(330, 14);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 13);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "PWM ON";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(330, 40);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(57, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "PWM OFF";
+            // 
+            // numericUpDown_pwmmax
+            // 
+            this.numericUpDown_pwmmax.Location = new System.Drawing.Point(481, 64);
+            this.numericUpDown_pwmmax.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numericUpDown_pwmmax.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_pwmmax.Name = "numericUpDown_pwmmax";
+            this.numericUpDown_pwmmax.Size = new System.Drawing.Size(126, 20);
+            this.numericUpDown_pwmmax.TabIndex = 24;
+            this.numericUpDown_pwmmax.Value = new decimal(new int[] {
+            40,
+            0,
+            0,
+            0});
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(330, 66);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(56, 13);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Timer Max";
+            // 
+            // button_setpwm
+            // 
+            this.button_setpwm.Location = new System.Drawing.Point(464, 103);
+            this.button_setpwm.Name = "button_setpwm";
+            this.button_setpwm.Size = new System.Drawing.Size(88, 24);
+            this.button_setpwm.TabIndex = 26;
+            this.button_setpwm.Text = "Set PWM";
+            this.button_setpwm.UseVisualStyleBackColor = true;
+            this.button_setpwm.Click += new System.EventHandler(this.button_setpwm_Click);
+            // 
+            // numericUpDown_pretrig
+            // 
+            this.numericUpDown_pretrig.Location = new System.Drawing.Point(109, 201);
+            this.numericUpDown_pretrig.Maximum = new decimal(new int[] {
+            349520,
+            0,
+            0,
+            0});
+            this.numericUpDown_pretrig.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_pretrig.Name = "numericUpDown_pretrig";
+            this.numericUpDown_pretrig.Size = new System.Drawing.Size(126, 20);
+            this.numericUpDown_pretrig.TabIndex = 27;
+            this.numericUpDown_pretrig.Value = new decimal(new int[] {
+            1875,
+            0,
+            0,
+            0});
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(22, 203);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(55, 13);
+            this.label12.TabIndex = 28;
+            this.label12.Text = "Pre trigger";
             // 
             // DischargeFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(688, 390);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.numericUpDown_pretrig);
+            this.Controls.Add(this.button_setpwm);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.numericUpDown_pwmmax);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.numericUpDown_pwmoff);
+            this.Controls.Add(this.numericUpDown_pwmon);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.numericUpDown_dualsplit);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.numericUpDown_selectedfixture);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.numericUpDown_node);
             this.Controls.Add(this.button_unlock);
@@ -296,6 +526,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_duration3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_duration4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_node)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_selectedfixture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_dualsplit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_pwmon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_pwmoff)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_pwmmax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_pretrig)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -319,5 +555,18 @@
         private System.Windows.Forms.Button button_unlock;
         private System.Windows.Forms.NumericUpDown numericUpDown_node;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown_selectedfixture;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numericUpDown_dualsplit;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown numericUpDown_pwmon;
+        private System.Windows.Forms.NumericUpDown numericUpDown_pwmoff;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.NumericUpDown numericUpDown_pwmmax;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button_setpwm;
+        private System.Windows.Forms.NumericUpDown numericUpDown_pretrig;
+        private System.Windows.Forms.Label label12;
     }
 }
--- CanMonitor/DischargeBoardTest/DischargeFrm.cs
+++ CanMonitor/DischargeBoardTest/DischargeFrm.cs
@@ -25,16 +25,20 @@ namespace DischargeBoardTest
         {
 
             byte node = (byte)numericUpDown_node.Value;
-            UInt32[] duration = new UInt32[4];
+            UInt32[] duration = new UInt32[6];
             duration[0] = (UInt32)numericUpDown_duration1.Value;
             duration[1] = (UInt32)numericUpDown_duration2.Value;
             duration[2] = (UInt32)numericUpDown_duration3.Value;
             duration[3] = (UInt32)numericUpDown_duration4.Value;
+            duration[4] = (UInt32)numericUpDown_dualsplit.Value;
+            duration[6] = (UInt32)numericUpDown_pretrig.Value;
 
             lco.SDOwrite(node, 0x2150, 1, duration[0],null);
             lco.SDOwrite(node, 0x2150, 2, duration[1], null);
             lco.SDOwrite(node, 0x2150, 3, duration[2], null);
             lco.SDOwrite(node, 0x2150, 4, duration[3], null);
+            lco.SDOwrite(node, 0x2150, 5, duration[4], null);
+            lco.SDOwrite(node, 0x2150, 6, duration[5], null);
 
         }
 
@@ -43,6 +47,7 @@ namespace DischargeBoardTest
             byte node = (byte)numericUpDown_node.Value;
             byte mode = (byte)comboBox_mode.SelectedIndex;
 
+            mode++; //combobox index starts at 0 we need to start at 1
             lco.SDOwrite(node, 0x2151, 1, mode, null);
         }
 
@@ -56,8 +61,33 @@ namespace DischargeBoardTest
         private void button_fire_Click(object sender, EventArgs e)
         {
             byte node = (byte)numericUpDown_node.Value;
+            byte fixture = (byte)numericUpDown_selectedfixture.Value;
+
+            lco.SDOwrite(node, 0x2155, 0, fixture, null);
+
             UInt32 fire = 0x46495200;
+            fire |= fixture;
             lco.SDOwrite(node, 0x2154, 0, fire, null);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_setpwm_Click(object sender, EventArgs e)
+        {
+
+            byte node = (byte)numericUpDown_node.Value;
+
+            UInt16 on = (UInt16)numericUpDown_pwmon.Value;
+            UInt16 off = (UInt16)numericUpDown_pwmoff.Value;
+            UInt16 tmr = (UInt16)numericUpDown_pwmmax.Value;
+
+            lco.SDOwrite(node, 0x2152, 1, on, null);
+            lco.SDOwrite(node, 0x2152, 2, off, null);
+            lco.SDOwrite(node, 0x2152, 3, tmr, null);
+
         }
     }
 }
